03bcd003c59343e9a18aebc76d0a5d43ec23bdd7
Matheus Lino <7matheuslino@gmail.com>
2020-05-23 11:23:13 -0300

    Finalizing class 'API de Contexto'

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
 
 import logoImg from '../../assets/logo.svg';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -14,6 +16,8 @@ import { Container, Content, Background } from './styles';
 
 const SingnIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
